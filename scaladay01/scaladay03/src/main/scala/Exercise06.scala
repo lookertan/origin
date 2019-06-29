fea8b22ad043f52509c4170177516193a7316f9e
jerry <380051012@qq.com>
2019-06-29 09:57:09 +0800

    first111

--- scaladay01/scaladay03/src/main/scala/Exercise06.scala
+++ scaladay01/scaladay03/src/main/scala/Exercise06.scala
@@ -1,3 +1,37 @@
 object Exercise06 {
+  // 程序的入口：
+  def main(args: Array[String]): Unit = {
+    val oralceDB = new OracleDB with Operate3
+    oralceDB.insert(100)
+
+    val mysql = new MySQL3 with Operate3
+    mysql.insert(200)
+
+    val l3 = new MySQL3_ with Operate3 {
+      override def say(): Unit = {
+        println("这么麻烦")
+      }
+    }
+    l3.insert(122)
+
+  }
+}
+
+trait Operate3{
+  def insert(id :Int):Unit= {
+    println("插入数据为 = " + id)
+  }
+}
+
+class OracleDB{
+
+}
+
+abstract class MySQL3{
+
+}
+
+abstract class MySQL3_{
+  def say()
 
 }
